06bc0f8252a5a08cea499d11359067b5adfc141b
wkflanders <wflan@seas.upenn.edu>
2025-01-20 08:46:16 -0500

    privy auth init

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,30 +10,9 @@ export default function Login() {
     const router = useRouter();
     const { getAccessToken } = usePrivy();
     const { login } = useLogin({
-        onComplete: async ({ isNewUser }) => {
+        onComplete: async ({ user, isNewUser, wasAlreadyAuthenticated, loginMethod, }) => {
             try {
-                const token = await getAccessToken();
-                if (!token) {
-                    throw new Error('Failed to get access token');
-                }
 
-                const res = await fetch('/api/auth/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,  // pass Privy JWT
-                    },
-                    body: JSON.stringify([])
-                });
-
-                if (!res.ok) {
-                    throw new Error('Login failed on server');
-                }
-                if (isNewUser) {
-                    router.push('/onboard');
-                } else {
-                    router.push('/dashboard');
-                }
             } catch (error) {
                 console.error('Login error: ', error);
             }
