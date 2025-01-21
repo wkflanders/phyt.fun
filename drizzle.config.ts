f9569c05e899dd7bb02e25917fdb6a603eefe9bb
wkflanders <wflan@seas.upenn.edu>
2025-01-21 13:56:25 -0500

    drizzel client

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { defineConfig } from "drizzle-kit";
+
+config({ path: '.env' });
+
+export default defineConfig({
+    schema: "./database/schema.ts",
+    out: "./migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+    introspect: {
+        casing: 'preserve'
+    }
+});
