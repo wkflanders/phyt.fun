31078fb7700d38c8205fa2874d9b856024a433a0
wkflanders <wflan@seas.upenn.edu>
2025-01-22 05:16:07 -0500

    add next support for imagekit

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "rsg5uys7zq.ufs.sh",
       },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io"
+      }
     ],
   },
 };
