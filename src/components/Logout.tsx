ec9e8a8dbef8fd58f34b344bd9548f2c26d09bca
wkflanders <wflan@seas.upenn.edu>
2025-01-22 12:59:32 -0500

    logout button

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -2,13 +2,22 @@
 import React from 'react';
 import { Button } from './ui/button';
 import { usePrivy } from '@privy-io/react-auth';
+import { useRouter } from 'next/navigation';
 
 const Logout = () => {
     const { ready, logout } = usePrivy();
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        await logout();
+        router.push('/');
+    };
     return (
-        <Button onClick={logout}>
-            click me
-        </Button>
+        ready && (
+            <Button onClick={handleLogout}>
+                logout
+            </Button>
+        )
     );
 };
 
