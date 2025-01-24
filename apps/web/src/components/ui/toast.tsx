ec5164050170f0afca8c9e3868883ecd21fd9e1f
wkflanders <wflan@seas.upenn.edu>
2025-01-23 22:21:35 -0500

    fix toast text styles

--- apps/web/src/components/ui/toast.tsx
+++ apps/web/src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-phyt_bg text-foreground",
+        default: "border bg-phyt_bg text-phyt_text",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
