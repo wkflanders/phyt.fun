cffd035ac57e8b43a5637bcb52ea049ffc293dba
wkflanders <wflan@seas.upenn.edu>
2025-01-23 22:21:24 -0500

    check ready for privy

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,26 +1,33 @@
 "use client";
 
 import React from 'react';
+import { usePrivy } from '@privy-io/react-auth';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 
 export default function Header() {
     const pathname = usePathname();
+    const { user, ready } = usePrivy();
 
     return (
-        <header className="my-10 flex justify-between gap-5 text-white">
-            <Link href="/">
-                Phyt
-            </Link>
-            <ul className="flex flex-row items-center gapy-8">
-                <li>
-                    <Link href="/packs" className={cn("text-base cursor-pointer capitalize", pathname === '/packs' ? 'text-phyt_red' : 'text-phyt_blue')}>
-                        Packs
-                    </Link>
+        ready && (
+            <header className="my-10 flex justify-between gap-5 text-white">
+                <Link href="/">
+                    Phyt
+                </Link>
+                <ul className="flex flex-row items-center gapy-8">
+                    <li>
+                        <Link href="/packs" className={cn("text-base cursor-pointer capitalize", pathname === '/packs' ? 'text-phyt_red' : 'text-phyt_blue')}>
+                            Packs
+                        </Link>
 
-                </li>
-            </ul>
-        </header>
+                    </li>
+                    <li>
+                        user.id
+                    </li>
+                </ul>
+            </header>
+        )
     );
 }
