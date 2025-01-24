db4a35f33baf2b845bd3f0c62a6b46710508a5d5
wkflanders <wflan@seas.upenn.edu>
2025-01-23 20:01:39 -0500

    fix type import

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { ApiError } from "../../types";
+import { ApiError } from "@phyt/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
