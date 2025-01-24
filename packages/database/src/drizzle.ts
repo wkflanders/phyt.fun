613f0e7c398afaac3bf6ed9566ca5493e80ffda8
wkflanders <wflan@seas.upenn.edu>
2025-01-23 20:50:50 -0500

    fix db dist output

--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
-config({ path: ".env" });
+config({ path: "../../.env" });
 
 const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
