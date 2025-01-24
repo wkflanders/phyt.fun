51fde9fbba90289f9b6164a29246afde73193e99
wkflanders <wflan@seas.upenn.edu>
2025-01-23 19:10:04 -0500

    fix database strict type

--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -1,10 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 import { config } from "dotenv";
-import { eq, and } from 'drizzle-orm';
 
 config({ path: ".env" });
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
-export { db, sql, eq, and };
+const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
+
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,4 @@
 export * from './schema';
 export * from './relations';
 export * from './drizzle';
+export { eq, and, desc, asc } from 'drizzle-orm';
