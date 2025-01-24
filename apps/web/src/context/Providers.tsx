8b986d2ea8eb5ec23b264ca88a19bb941a0b25fd
wkflanders <wflan@seas.upenn.edu>
2025-01-24 05:29:05 -0500

    wagmi provider

--- apps/web/src/context/Providers.tsx
+++ apps/web/src/context/Providers.tsx
@@ -3,27 +3,21 @@
 import React from 'react';
 import { PrivyProvider } from '@privy-io/react-auth';
 import { QueryProvider } from './QueryProvider';
+import { WagmiProvider } from "@privy-io/wagmi";
+import { config } from "@/lib/wagmi";
+import { privyConfig } from '@/lib/privyConfig';
 
 export default function Providers({ children }: { children: React.ReactNode; }) {
     return (
 
         <PrivyProvider
             appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-            config={{
-                // Customize Privy's appearance in your app
-                appearance: {
-                    theme: 'dark',
-                    accentColor: '#00F6FB',
-                    logo: 'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkF3xIgVJvDkJmxutZaE5dj0qbGXU9Kl6ASci7P',
-                },
-                // Create embedded wallets for users who don't have a wallet
-                embeddedWallets: {
-                    createOnLogin: 'users-without-wallets',
-                },
-            }}
+            config={privyConfig}
         >
             <QueryProvider>
-                {children}
+                <WagmiProvider config={config}>
+                    {children}
+                </WagmiProvider>
             </QueryProvider>
         </PrivyProvider>
 
