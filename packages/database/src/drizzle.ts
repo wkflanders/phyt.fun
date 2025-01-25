33ca1f10e55241265a46e2dcfd9bb84a4d86f04c
wkflanders <wflan@seas.upenn.edu>
2025-01-24 19:07:06 -0500

    fix drizzle schema types

--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon, NeonQueryFunction } from "@neondatabase/serverless";
+import * as schema from './schema';
 import { config } from "dotenv";
 
 config({ path: "../../.env" });
 
 const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
 
