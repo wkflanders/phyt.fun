825cdd6c45af9808bd2f0b90ef9281e9b7c5b3e4
wkflanders <wflan@seas.upenn.edu>
2025-01-29 07:06:24 -0500

    pack routes

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { userRouter } from './users';
+import { packRouter } from './packs';
 
 const router: Router = Router();
 
 router.use('/users', userRouter);
+
+router.use('/packs', packRouter);
 
 export default router;
