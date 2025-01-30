893be2ca5be6c6ea25ac5882057a58aa4b5388b5
wkflanders <wflan@seas.upenn.edu>
2025-01-29 22:04:43 -0500

    fix imports

--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -3,8 +3,7 @@ import { neon, NeonQueryFunction } from "@neondatabase/serverless";
 import * as schema from './schema';
 import { config } from "dotenv";
 
-config({ path: "../../.env" });
+config({ path: "./env" });
 
 const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
-
