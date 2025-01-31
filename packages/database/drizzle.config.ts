a82519a12dc645e7833ac3da7fbbb0476edff70b
wkflanders <wflan@seas.upenn.edu>
2025-01-31 13:50:16 -0500

    fix env for database

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env' });
+config({ path: '../../.env' });
 
 export default defineConfig({
     schema: "./src/schema.ts",
--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -1,10 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import { config } from "dotenv";
 
-config({ path: "./env" });
+config({ path: '../../.env' });
 
 const sql = neon<boolean, boolean>(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
