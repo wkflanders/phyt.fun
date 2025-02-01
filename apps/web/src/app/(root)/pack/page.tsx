ae2544648f0c273ef7008f185f6462187a3f8b2a
wkflanders <wflan@seas.upenn.edu>
2025-02-01 11:22:14 -0500

    remove stupid ass header

--- apps/web/src/app/(root)/pack/page.tsx
+++ apps/web/src/app/(root)/pack/page.tsx
@@ -4,11 +4,7 @@ import PackPurchase from '@/components/Packs/PackPurchase';
 export default function PackPage() {
     return (
         <div className="flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold text-phyt_text">Runner Packs</h1>
-                </div>
-
+            <div className=" max-w-7xl mx-auto py-40 px-4 sm:px-6 lg:px-8">
                 <PackPurchase />
             </div>
         </div>
