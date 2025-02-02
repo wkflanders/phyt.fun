769d1344928c045d41c4ebd92a66ea7805dbc428
wkflanders <wflan@seas.upenn.edu>
2025-02-02 15:16:02 -0500

    export db functions

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,4 +2,4 @@ export * from './schema';
 export * from './relations';
 export * from './drizzle';
 export * from './ws';
-export { eq, and, desc, asc, or } from 'drizzle-orm';
+export { eq, and, desc, asc, or, not, sql } from 'drizzle-orm';
