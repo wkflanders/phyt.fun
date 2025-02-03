647dad51c0777c4f7f87a101acdf431ca2285fea
wkflanders <wflan@seas.upenn.edu>
2025-02-03 18:15:42 -0500

    marketplace routes

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { userRouter } from './users';
 import { packRouter } from './packs';
+import { marketplaceRouter } from './marketplace';
 
 const router: Router = Router();
 
 router.use('/users', userRouter);
 
 router.use('/packs', packRouter);
+
+router.use('/marketplace', marketplaceRouter);
 
 export default router;
