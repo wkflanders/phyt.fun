c1ecaa44ccb43aa2676c4ca0ca00c9e1bdb7520e
wkflanders <wflan@seas.upenn.edu>
2025-02-03 06:33:12 -0500

    remove abi from repo

--- packages/contracts/abi/index.d.ts
+++
@@ -1,6 +0,0 @@
-import MinterAbi from './Minter.json';
-import PhytCardsAbi from './PhytCards.json';
-import ExecutorAbi from './Executor.json';
-import ExchangeAbi from './Exchange.json';
-export { MinterAbi, PhytCardsAbi, ExecutorAbi, ExchangeAbi };
-//# sourceMappingURL=index.d.ts.map
