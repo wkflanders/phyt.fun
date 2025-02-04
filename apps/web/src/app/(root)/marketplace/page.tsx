d165d1f82c75dab421f5c3a2c3df5bff62d51c69
wkflanders <wflan@seas.upenn.edu>
2025-02-04 15:18:04 -0500

    marketplace page

--- apps/web/src/app/(root)/marketplace/page.tsx
+++ apps/web/src/app/(root)/marketplace/page.tsx
@@ -7,10 +7,6 @@ export default function MarketplacePage() {
     return (
         <div className="flex-1 overflow-y-auto">
             <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-                <div className="text-center mb-12">
-                    <h1 className="text-4xl font-bold text-phyt_text mb-4">NFT Marketplace</h1>
-                </div>
-
                 <Marketplace />
             </div>
         </div>
