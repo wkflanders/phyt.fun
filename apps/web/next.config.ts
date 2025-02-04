a11c19f5abf271bea9ef6c11a23190fd450071b1
wkflanders <wflan@seas.upenn.edu>
2025-02-04 16:24:33 -0500

    cloudfront serve urls

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -13,20 +13,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "d9dbhfllhp347.cloudfront.net",
-        pathname: "/prod/avatars/**",
-      },
-      {
-        protocol: "https",
-        hostname: "d1o7ihod05ar3g.cloudfront.net"
-      },
-      {
-        protocol: "https",
         hostname: "d5mhfgomyfg7p.cloudfront.net"
       },
       {
         protocol: "https",
-        hostname: "https://dc1foe9a4vvf0.cloudfront.net"
+        hostname: "dc1foe9a4vvf0.cloudfront.net"
       },
     ],
     dangerouslyAllowSVG: true,
