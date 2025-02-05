ee190c93b3993325f910bb84928e584260815616
wkflanders <wflan@seas.upenn.edu>
2025-02-05 01:20:06 -0500

    inventory component

--- apps/web/src/components/Profile/Inventory.tsx
+++ apps/web/src/components/Profile/Inventory.tsx
@@ -4,9 +4,8 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { useGetUserCards } from '@/hooks/use-get-user-cards';
 import { Loader2 } from 'lucide-react';
-import { CardRarity, CardWithMetadata, User } from '@phyt/types';
-import { OrderBookModal } from './OrderBook';
-import { CardModal } from './CardModal';
+import { CardWithMetadata, User } from '@phyt/types';
+import { CardModal } from '../CardModal';
 
 interface InventoryProps {
     user: User;
@@ -17,6 +16,7 @@ export const Inventory = ({
 }: InventoryProps) => {
     const { data: cards, isLoading, error } = useGetUserCards();
     const [selectedCard, setSelectedCard] = useState<any>(null);
+
     if (isLoading) {
         return (
             <div className="flex-1 flex items-center justify-center">
@@ -34,23 +34,20 @@ export const Inventory = ({
     }
 
     return (
-        <div className="flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto py-8 px-6">
-                <div className="grid grid-cols-3">
+        <div className="h-full overflow-y-auto">
+            <div className="container mx-auto p-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
                     {cards?.map((card: CardWithMetadata) => (
                         <div
                             key={card.id}
-                            onClick={() => {
-                                setSelectedCard(card);
-                            }}
-                            className="cursor-pointer transition-transform hover:scale-105"
+                            onClick={() => setSelectedCard(card)}
+                            className="w-60 h-96 relative cursor-pointer"
                         >
                             <Image
                                 src={card.metadata.image_url}
                                 alt={`Card ${card.metadata.token_id}`}
-                                width={400}
-                                height={600}
-                                className="rounded-lg w-1/2 object-cover cursor-pointer transition-transform hover:scale-105"
+                                fill
+                                className="rounded-lg object-contain p-1 hover:scale-105 transition-transform duration-200"
                             />
                         </div>
                     ))}
