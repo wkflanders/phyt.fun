561f0edccf79d65151486d85ed0c277e3b6194f6
wkflanders <wflan@seas.upenn.edu>
2025-02-05 04:32:12 -0500

    marketplace page padding

--- apps/web/src/app/(root)/marketplace/page.tsx
+++ apps/web/src/app/(root)/marketplace/page.tsx
@@ -6,7 +6,7 @@ import Marketplace from '@/components/Marketplace/Marketplace';
 export default function MarketplacePage() {
     return (
         <div className="flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto">
                 <Marketplace />
             </div>
         </div>
