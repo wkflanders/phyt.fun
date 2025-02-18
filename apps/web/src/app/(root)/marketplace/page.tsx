e2a4c70d56d369c770cf370f6ff25ef8bdaa8998
wkflanders <wflan@seas.upenn.edu>
2025-02-18 14:40:15 -0500

    fix marketplace styling

--- apps/web/src/app/(root)/marketplace/page.tsx
+++ apps/web/src/app/(root)/marketplace/page.tsx
@@ -6,7 +6,7 @@ import Marketplace from '@/components/Marketplace/Marketplace';
 export default function MarketplacePage() {
     return (
         <div className="flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-4xl mx-auto">
                 <Marketplace />
             </div>
         </div>
