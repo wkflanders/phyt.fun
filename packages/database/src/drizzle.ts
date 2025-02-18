5dd7f2162d7a0ac52f36c76175412eaca5ba7932
wkflanders <wflan@seas.upenn.edu>
2025-02-18 16:37:17 -0500

    fix connection string

--- packages/database/src/drizzle.ts
+++ packages/database/src/drizzle.ts
@@ -6,11 +6,7 @@ import { config } from 'dotenv';
 config({ path: '../../.env' });
 
 const pool = new Pool({
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT || '5432'),
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    connectionString: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, { schema });
