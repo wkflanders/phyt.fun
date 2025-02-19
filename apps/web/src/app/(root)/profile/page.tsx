452619884fe780c139defecbb04a35f54e87dc8e
wkflanders <wflan@seas.upenn.edu>
2025-02-19 04:16:39 -0500

    fix page spacing affecting aside column

--- apps/web/src/app/(root)/profile/page.tsx
+++ apps/web/src/app/(root)/profile/page.tsx
@@ -3,8 +3,10 @@ import { Profile } from '@/components/Profile/Profile';
 
 export default function ProfilePage() {
     return (
-        <div className="flex justify-center items-center align-middle w-10/12">
-            <Profile />
+        <div className="flex-1 overflow-y-auto">
+            <div className="max-w-4xl mx-auto">
+                <Profile />
+            </div>
         </div>
     );
 };
