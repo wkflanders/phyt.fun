bb2733f91965496da257cd163d21b9fba4e4b30b
wkflanders <wflan@seas.upenn.edu>
2025-02-20 22:55:04 -0500

    runner router

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -3,6 +3,7 @@ import { userRouter } from './users';
 import { packRouter } from './packs';
 import { marketplaceRouter } from './marketplace';
 import { runRouter } from './runs';
+import { runnerRouter } from './runners';
 
 const router: Router = Router();
 
@@ -13,5 +14,7 @@ router.use('/packs', packRouter);
 router.use('/marketplace', marketplaceRouter);
 
 router.use('/workouts/runs', runRouter);
+
+router.use('/runners', runnerRouter);
 
 export default router;
