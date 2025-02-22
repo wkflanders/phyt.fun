689ca60754469df69c7257a6d51b268a44a3beb6
wkflanders <wflan@seas.upenn.edu>
2025-02-22 13:43:31 -0500

    remove debug statement

--- apps/server/src/services/userServices.ts
+++ apps/server/src/services/userServices.ts
@@ -17,7 +17,6 @@ export const userService = {
                 .limit(1);
 
             if (!user) throw new NotFoundError('User not found');
-            console.log(user);
             return user;
         } catch (error) {
             if (error instanceof NotFoundError || error instanceof ValidationError) {
