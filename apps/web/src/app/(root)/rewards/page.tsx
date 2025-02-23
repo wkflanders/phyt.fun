6eeb6de3eb22a179b0ec221d1b6f2d827ca3beba
wkflanders <wflan@seas.upenn.edu>
2025-02-23 17:06:23 -0500

    rewards page init

---
+++ apps/web/src/app/(root)/rewards/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function RewardsPage() {
+    return (
+        <div className="flex-1 overflow-y-auto">
+            <div className="max-w-4xl mx-auto">
+            </div>
+        </div>
+    );
+};
