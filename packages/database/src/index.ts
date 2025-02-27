2bac06c1495ee4bb6dcb74d6585d45d1489889ff
wkflanders <wflan@seas.upenn.edu>
2025-02-27 15:15:21 -0500

    export db count function

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,4 +2,4 @@ export * from './schema';
 export * from './relations';
 export * from './drizzle';
 export * from './transactions';
-export { eq, and, desc, asc, or, not, sql, gt, lt, gte, lte, ne, like, isNull } from 'drizzle-orm';
+export { eq, and, desc, asc, or, not, sql, gt, lt, gte, lte, ne, like, isNull, count } from 'drizzle-orm';
