ea11de6fe90ec5a2cec7688e91a07f82453c776f
wkflanders <wflan@seas.upenn.edu>
2025-03-21 18:36:34 -0400

    auth check middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,11 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { handleAuth } from './middleware/auth';
 
-
-export function middleware(req: NextRequest) {
-    return handleAuth(req);
-}
+const bypassAuthParams = ['privy_oauth_code', 'privy_oauth_state', 'privy_oauth_provider'];
 
 export const config = {
-    matcher: ['/((?!api|_next|_static|_vercel|favicon.ico|sitemap.xml).*)',],
+    matcher: '/',
 };
+
+export function middleware(req: NextRequest) {
+    for (const param of bypassAuthParams) {
+        if (req.nextUrl.searchParams.get(param)) {
+            return NextResponse.next();
+        }
+    }
+
+    if (req.nextUrl.pathname.includes('/refresh')) return NextResponse.next();
+
+    const cookieAuthToken = req.cookies.get('privy-token');
+    const cookieSession = req.cookies.get('privy-session');
+
+    const definitelyAuthenticated = Boolean(cookieAuthToken);
+
+    const maybeAuthenticated = Boolean(cookieSession);
+
+    if (!definitelyAuthenticated && maybeAuthenticated) {
+        const refreshUrl = req.nextUrl.clone();
+        refreshUrl.pathname = '/refresh';
+        refreshUrl.searchParams.set('redirect_uri', req.nextUrl.pathname + req.nextUrl.search);
+        return NextResponse.redirect(refreshUrl);
+    }
+
+    return NextResponse.next();
+}
