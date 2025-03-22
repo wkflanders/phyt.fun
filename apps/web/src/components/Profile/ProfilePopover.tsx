2b97c4096dbc2b4a39d44ce7dd74979f01405ff2
wkflanders <wflan@seas.upenn.edu>
2025-03-22 06:38:38 -0400

    add logout to profile popover

--- apps/web/src/components/Profile/ProfilePopover.tsx
+++ apps/web/src/components/Profile/ProfilePopover.tsx
@@ -3,12 +3,15 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useLogout } from "@privy-io/react-auth";
+import { useRouter } from "next/navigation";
 import {
     Popover,
     PopoverTrigger,
     PopoverContent,
 } from "@/components/ui/popover";
 import { Button } from "../ui/button";
+import { LogOut } from "lucide-react";
 
 type ProfilePopoverProps = {
     avatarUrl?: string;
@@ -18,12 +21,27 @@ type ProfilePopoverProps = {
 const DEFAULT_AVATAR = 'https://rsg5uys7zq.ufs.sh/f/AMgtrA9DGKkFuVELmbdSRBPUEIciTL7a2xg1vJ8ZDQh5ejut';
 
 export const ProfilePopover = ({ avatarUrl, username }: ProfilePopoverProps) => {
+    const router = useRouter();
+    const { logout } = useLogout({
+        onSuccess: () => {
+            router.push("/login");
+        },
+    });
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error("Error logging out", error);
+        }
+    };
+
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button
                     variant="ghost"
-                    className="flex items-center gap-4 px-4 py-2 transition-colors duration-200 group rounded-xl hover:bg-black/20 hover:cursor-pointer"
+                    className="flex items-center gap-4 px-4 py-2 transition-colors duration-200 group rounded-xl data-[state=open]:bg-black/20 hover:bg-black/20 hover:cursor-pointer"
                 >
                     <Image
                         src={avatarUrl || DEFAULT_AVATAR}
@@ -71,9 +89,9 @@ export const ProfilePopover = ({ avatarUrl, username }: ProfilePopoverProps) => 
                         Help
                     </Link>
                     <hr className="my-1" />
-                    <Link href="/profile" className="p-3 hover:bg-black/20 rounded">
-                        Logout
-                    </Link>
+                    <button onClick={handleLogout} className="p-3 text-red-500 flex items-center gap-2 hover:bg-black/20 rounded">
+                        <LogOut size={16} /> Logout
+                    </button>
                 </div>
             </PopoverContent>
         </Popover>
