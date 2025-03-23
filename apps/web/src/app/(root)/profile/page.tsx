b611a72e556273f678ea9394b8deae3f058942ec
wkflanders <wflan@seas.upenn.edu>
2025-03-23 10:14:13 -0400

    fix profile page spacing

--- apps/web/src/app/(root)/profile/page.tsx
+++ apps/web/src/app/(root)/profile/page.tsx
@@ -4,7 +4,7 @@ import { Profile } from '@/components/Profile/Profile';
 export default function ProfilePage() {
     return (
         <div className="flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto">
+            <div className="pl-12">
                 <Profile />
             </div>
         </div>
