c6348b47a693c82cac8eb31c59d13e9b5d0ecac0
wkflanders <wflan@seas.upenn.edu>
2025-03-24 14:11:41 -0400

    use leaderboard hook refactor

--- apps/web/src/hooks/use-leaderboard.ts
+++ apps/web/src/hooks/use-leaderboard.ts
@@ -1,12 +1,12 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { usePrivy } from "@privy-io/react-auth";
 import {
+    getManagerLeaderboard,
     getRunnerLeaderboard,
-    getUserLeaderboard,
     LEADERBOARD_RUNNER_QUERY_KEY,
     LEADERBOARD_USER_QUERY_KEY,
 } from '@/queries/leaderboard';
-import { LeaderboardQueryParams, RunnerLeaderboard, UserLeaderboard, ApiError } from '@phyt/types';
+import { LeaderboardQueryParams, RunnerLeaderboard, ManagerLeaderboard, ApiError } from '@phyt/types';
 
 export function useGetRunnerLeadeboard(params: LeaderboardQueryParams) {
     const { user: privyUser, getAccessToken } = usePrivy();
@@ -25,10 +25,10 @@ export function useGetRunnerLeadeboard(params: LeaderboardQueryParams) {
     });
 }
 
-export function useGetUserLeaderboard(params: LeaderboardQueryParams) {
+export function useGetManagerLeaderboard(params: LeaderboardQueryParams) {
     const { user: privyUser, getAccessToken } = usePrivy();
 
-    return useQuery<UserLeaderboard, ApiError>({
+    return useQuery<ManagerLeaderboard, ApiError>({
         queryKey: [LEADERBOARD_USER_QUERY_KEY, privyUser?.id],
         queryFn: async () => {
             if (!privyUser?.id) {
@@ -36,7 +36,7 @@ export function useGetUserLeaderboard(params: LeaderboardQueryParams) {
             }
             const token = await getAccessToken();
 
-            const leaderboardData = await getUserLeaderboard(params, token);
+            const leaderboardData = await getManagerLeaderboard(params, token);
             return leaderboardData;
         },
     });
