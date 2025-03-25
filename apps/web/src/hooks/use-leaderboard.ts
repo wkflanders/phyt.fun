e73354eeecff335a1ff17a470409deed5d5522a3
wkflanders <wflan@seas.upenn.edu>
2025-03-25 07:47:49 -0400

    use leaderboard hooks for singular manager and runner rankings

--- apps/web/src/hooks/use-leaderboard.ts
+++ apps/web/src/hooks/use-leaderboard.ts
@@ -2,21 +2,22 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { usePrivy } from "@privy-io/react-auth";
 import {
     getManagerLeaderboard,
+    getManagerStanding,
     getRunnerLeaderboard,
+    getRunnerStanding,
     LEADERBOARD_RUNNER_QUERY_KEY,
     LEADERBOARD_USER_QUERY_KEY,
+    MANAGER_STANDING_QUERY_KEY,
+    RUNNER_STANDING_QUERY_KEY,
 } from '@/queries/leaderboard';
-import { LeaderboardQueryParams, RunnerLeaderboard, ManagerLeaderboard, ApiError } from '@phyt/types';
+import { LeaderboardQueryParams, RunnerLeaderboard, ManagerLeaderboard, ApiError, ManagerStanding, RunnerStanding } from '@phyt/types';
 
 export function useGetRunnerLeadeboard(params: LeaderboardQueryParams) {
-    const { user: privyUser, getAccessToken } = usePrivy();
+    const { getAccessToken } = usePrivy();
 
     return useQuery<RunnerLeaderboard, ApiError>({
-        queryKey: [LEADERBOARD_RUNNER_QUERY_KEY, privyUser?.id],
+        queryKey: [LEADERBOARD_RUNNER_QUERY_KEY],
         queryFn: async () => {
-            if (!privyUser?.id) {
-                throw new Error("No authenticated user");
-            }
             const token = await getAccessToken();
 
             const leaderboardData = await getRunnerLeaderboard(params, token);
@@ -26,14 +27,11 @@ export function useGetRunnerLeadeboard(params: LeaderboardQueryParams) {
 }
 
 export function useGetManagerLeaderboard(params: LeaderboardQueryParams) {
-    const { user: privyUser, getAccessToken } = usePrivy();
+    const { getAccessToken } = usePrivy();
 
     return useQuery<ManagerLeaderboard, ApiError>({
-        queryKey: [LEADERBOARD_USER_QUERY_KEY, privyUser?.id],
+        queryKey: [LEADERBOARD_USER_QUERY_KEY],
         queryFn: async () => {
-            if (!privyUser?.id) {
-                throw new Error("No authenticated user");
-            }
             const token = await getAccessToken();
 
             const leaderboardData = await getManagerLeaderboard(params, token);
@@ -41,3 +39,31 @@ export function useGetManagerLeaderboard(params: LeaderboardQueryParams) {
         },
     });
 }
+
+export function useGetManagerStanding(id: string | number, params: LeaderboardQueryParams) {
+    const { getAccessToken } = usePrivy();
+
+    return useQuery<ManagerStanding, ApiError>({
+        queryKey: [MANAGER_STANDING_QUERY_KEY],
+        queryFn: async () => {
+            const token = await getAccessToken();
+
+            const managerStandingData = await getManagerStanding(id, params, token);
+            return managerStandingData;
+        },
+    });
+}
+
+export function useGetRunnerStanding(id: string | number, params: LeaderboardQueryParams) {
+    const { getAccessToken } = usePrivy();
+
+    return useQuery<RunnerStanding, ApiError>({
+        queryKey: [RUNNER_STANDING_QUERY_KEY],
+        queryFn: async () => {
+            const token = await getAccessToken();
+
+            const runnerStandingData = await getRunnerStanding(id, params, token);
+            return runnerStandingData;
+        },
+    });
+}   
