c0859dbc169e40cbd2c9722da79bde78cb2d4fe8
wkflanders <wflan@seas.upenn.edu>
2025-03-28 15:39:53 -0400

    fixing tsconfig package imports

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,13 +1,7 @@
 import { nextJsConfig } from '@phyt/eslint-config/next-js';
-import nextPlugin from '@next/eslint-plugin-next';
  
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...nextJsConfig,
-  {
-    plugins: {
-      '@next/next': nextPlugin,
-    }
-  },
   // Other configurations
 ];
