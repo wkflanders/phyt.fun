d1249f8c1f0c7a4983563cc8df44a16751ef1de6
wkflanders <wflan@seas.upenn.edu>
2025-03-28 10:35:46 -0400

    web linter

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,16 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended"),
+import { nextJsConfig } from '@phyt/eslint-config/next-js';
+ 
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...nextJsConfig,
+  // Other configurations
 ];
-
-export default eslintConfig;
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -14,36 +14,94 @@ import { config as baseConfig } from "./base.js";
  * */
 export const nextJsConfig = [
   ...baseConfig,
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
-    languageOptions: {
-      ...pluginReact.configs.flat.recommended.languageOptions,
-      globals: {
-        ...globals.serviceworker,
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...pluginReact.configs.recommended,
+    settings: {
+      "import/resolver": {
+        typescript: true,
       },
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      'react/no-access-state-in-setstate': 'error',
+      'react/no-array-index-key': 'warn',
+      'react/no-children-count': 'warn',
+      'react/no-children-for-each': 'warn',
+      'react/no-children-map': 'warn',
+      'react/no-children-only': 'warn',
+      'react/no-children-to-array': 'warn',
+      'react/no-clone-element': 'warn',
+      'react/no-comment-textnodes': 'warn',
+      'react/no-component-will-mount': 'error',
+      'react/no-component-will-receive-props': 'error',
+      'react/no-component-will-update': 'error',
+      'react/no-context-provider': 'warn',
+      'react/no-create-ref': 'error',
+      'react/no-default-props': 'error',
+      'react/no-direct-mutation-state': 'error',
+      'react/no-duplicate-jsx-props': 'warn',
+      'react/no-duplicate-key': 'warn',
+      'react/no-forward-ref': 'warn',
+      'react/no-implicit-key': 'warn',
+      'react/no-missing-key': 'error',
+      'react/no-nested-component-definitions': 'error',
+      'react/no-redundant-should-component-update': 'error',
+      'react/no-set-state-in-component-did-mount': 'warn',
+      'react/no-set-state-in-component-did-update': 'warn',
+      'react/no-set-state-in-component-will-update': 'warn',
+      'react/no-string-refs': 'error',
+      'react/no-unsafe-component-will-mount': 'warn',
+      'react/no-unsafe-component-will-receive-props': 'warn',
+      'react/no-unsafe-component-will-update': 'warn',
+      'react/no-unstable-context-value': 'warn',
+      'react/no-unstable-default-props': 'warn',
+      'react/no-unused-class-component-members': 'warn',
+      'react/no-unused-state': 'warn',
+      'react/no-use-context': 'warn',
+      'react/no-useless-forward-ref': 'warn',
+      'react/use-jsx-vars': 'warn',
     },
   },
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
-      "@next/next": pluginNext,
+      "react-hooks": pluginReactHooks,
+  },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
     },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],  
+    ...pluginNext.configs.recommended,
     rules: {
       ...pluginNext.configs.recommended.rules,
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...pluginNext.configs["core-web-vitals"],
+    rules: {
       ...pluginNext.configs["core-web-vitals"].rules,
     },
   },
   {
-    plugins: {
-      "react-hooks": pluginReactHooks,
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+        React: "readonly",
+      },
     },
-    settings: { react: { version: "detect" } },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-      // React scope no longer necessary with new JSX transform.
-      "react/react-in-jsx-scope": "off",
-    },
-  },
+  }
 ];
