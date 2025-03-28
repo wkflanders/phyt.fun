b5ce96bed1e7a4e131b0b903bc219982c788e3a8
wkflanders <wflan@seas.upenn.edu>
2025-03-28 10:36:33 -0400

    base lint config

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import pluginImport from "eslint-plugin-import";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -12,7 +12,16 @@ import onlyWarn from "eslint-plugin-only-warn";
 export const config = [
   js.configs.recommended,
   eslintConfigPrettier,
-  ...tseslint.configs.recommended,
+ ... tseslint.configs.strictTypeChecked,
+ ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      // parserOptions: {
+      //   projectService: true,
+      //   tsconfigRootDir: import.meta.dirname,
+      // },
+    },
+  },
   {
     plugins: {
       turbo: turboPlugin,
@@ -23,7 +32,38 @@ export const config = [
   },
   {
     plugins: {
-      onlyWarn,
+      import: pluginImport,
+    },
+    rules: {
+      ...pluginImport.configs.recommended.rules,
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "import/first": "error",
+      "import/no-duplicates": "error",
+      "import/no-mutable-exports": "error",
+      "import/no-named-default": "error",
+      "import/no-self-import": "error",
+      "import/no-webpack-loader-syntax": "error",
+    },
+  },
+  {
+    rules: {
+      "no-console": [
+        "warn",
+        {
+          allow: ["error", "warn"],
+        },
+      ],
+      curly: "error",
+      'spaced-comment': [
+        'error',
+        'always',
+        {
+          markers: ['/'],
+        },
+      ],
+      "quotes": ["error", "single"],
+      "semi": ["error", "all"],
+      "indent": ["error", 2],
     },
   },
   {
