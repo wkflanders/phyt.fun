864eb3655f72f0465cbc8d450142a70025b79a17
wkflanders <wflan@seas.upenn.edu>
2025-03-29 11:44:30 -0400

    strange tsconfig reference error that fixed itself? leaving commented code just in case

--- packages/types/eslint.config.mjs
+++ packages/types/eslint.config.mjs
@@ -1,19 +1,19 @@
 import { config as baseConfig } from '@phyt/eslint-config/base';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import { URL } from 'node:url';
+// import { URL } from 'node:url';
 
 /** @type {import("eslint").Linter.Config} */
 export default [
     ...baseConfig,
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        ignores: ['eslint.config.mjs'],
-        languageOptions: {
-            parserOptions: {
-                project: ['./tsconfig.json'],
-                tsconfigRootDir: new URL('.', import.meta.url).pathname
-            }
-        }
-    },
+    // {
+    //     files: ['**/*.ts', '**/*.tsx'],
+    //     ignores: ['eslint.config.mjs'],
+    //     languageOptions: {
+    //         parserOptions: {
+    //             project: ['./tsconfig.json'],
+    //             tsconfigRootDir: new URL('.', import.meta.url).pathname
+    //         }
+    //     }
+    // },
     eslintConfigPrettier
 ];
