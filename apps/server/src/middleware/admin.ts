2a6a047c84dea30759599f4357b3f52e82ea0461
wkflanders <wflan@seas.upenn.edu>
2025-03-30 12:09:43 -0400

    admin auth middleware

--- apps/server/src/middleware/admin.ts
+++ apps/server/src/middleware/admin.ts
@@ -1,38 +1,26 @@
-import { db, eq, users } from '@phyt/database';
-import { HttpError, User, AdminRequest } from '@phyt/types';
+import { HttpError, AuthenticatedRequest } from '@phyt/types';
 import { Response, NextFunction } from 'express';
+
+import { userService } from '@/services/userServices';
 
 import { validateAuth } from './auth';
 
 export const validateAdmin = [
     validateAuth,
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    async (req: AdminRequest, res: Response, next: NextFunction) => {
+    async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
         try {
-            const privyId = req.body.user.id;
+            const privyId = req.body.auth.privy_id;
 
             if (!privyId) {
                 throw new HttpError('User ID not provided', 401);
             }
 
-            const user = await db
-                .select()
-                .from(users)
-                .where(eq(users.privy_id, privyId.toString()))
-                .limit(1)
-                .then((results) =>
-                    results.length > 0 ? (results[0] as User) : null
-                );
-
-            if (user === null) {
-                throw new HttpError('User not found', 404);
-            }
+            const user = await userService.getUserByPrivyId(privyId);
 
             if (user.role !== 'admin') {
                 throw new HttpError('Unauthorized', 404);
             }
-
-            req.body.user = user;
 
             next();
         } catch (error) {
