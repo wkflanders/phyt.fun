327e6b6e9b69e8727f524a3db3c38018c1475750
wkflanders <wflan@seas.upenn.edu>
2025-03-31 16:17:30 -0400

    fix marketplace route types

--- apps/server/src/routes/marketplace.ts
+++ apps/server/src/routes/marketplace.ts
@@ -1,11 +1,16 @@
 import {
     HttpError,
     CreateListingRequestBody,
-    AuthenticatedBody,
     ListedBidRequestBody,
-    OpenBidRequestBody
+    OrderBook,
+    UserBids,
+    OpenBidRequestBody,
+    MarketListing,
+    OpenBid,
+    Listing,
+    ListingBid
 } from '@phyt/types';
-import express, { Router, Request, Response } from 'express';
+import express, { Request, Router, Response } from 'express';
 
 import { toStringValue } from '@/lib/utils';
 import { openBidSchema, bidSchema, listingSchema } from '@/lib/validation';
@@ -15,54 +20,70 @@ import { marketplaceService } from '@/services/marketplaceServices';
 
 const router: Router = express.Router();
 
-router.get('/listings', async (req: Request, res: Response) => {
-    const { minPrice, maxPrice, rarity, sort } = req.query;
+router.use(validateAuth);
 
-    const minPriceStr = toStringValue(minPrice);
-    const maxPriceStr = toStringValue(maxPrice);
+router.get(
+    '/listings',
+    async (
+        req: Request<
+            Record<string, never>, // req params
+            MarketListing[], // ressponse body
+            Record<string, never>, // req body
+            {
+                minPrice?: string | string[];
+                maxPrice?: string | string[]; // req query
+                rarity?: string | string[];
+                sort?: string | string[];
+            }
+        >,
+        res: Response<MarketListing[]>
+    ) => {
+        const { minPrice, maxPrice, rarity, sort } = req.query;
 
-    let rarityArray: string[] | undefined;
-    if (rarity != null) {
-        if (Array.isArray(rarity)) {
-            rarityArray = rarity.map((item) => {
-                const str = toStringValue(item);
-                return str ?? '';
-            });
-        } else {
-            const rarityVal = toStringValue(rarity);
-            rarityArray = rarityVal !== undefined ? [rarityVal] : undefined;
+        const minPriceStr = toStringValue(minPrice);
+        const maxPriceStr = toStringValue(maxPrice);
+
+        let rarityArray: string[] | undefined;
+        if (rarity != null) {
+            if (Array.isArray(rarity)) {
+                rarityArray = rarity.map((item) => {
+                    const str = toStringValue(item);
+                    return str ?? '';
+                });
+            } else {
+                const rarityVal = toStringValue(rarity);
+                rarityArray = rarityVal !== undefined ? [rarityVal] : undefined;
+            }
         }
+
+        const sortStr = toStringValue(sort) as
+            | 'price_asc'
+            | 'price_desc'
+            | 'created_at'
+            | undefined;
+
+        const listings = await marketplaceService.getListings({
+            minPrice: minPriceStr,
+            maxPrice: maxPriceStr,
+            rarity: rarityArray,
+            sort: sortStr
+        });
+
+        res.status(200).json(listings);
     }
-
-    const sortStr = toStringValue(sort) as
-        | 'price_asc'
-        | 'price_desc'
-        | 'created_at'
-        | undefined;
-
-    const listings = await marketplaceService.getListings({
-        minPrice: minPriceStr,
-        maxPrice: maxPriceStr,
-        rarity: rarityArray,
-        sort: sortStr
-    });
-
-    res.status(200).json(listings);
-});
+);
 
 // Create a new listing
 router.post(
     '/listing',
-    validateAuth,
     validateSchema(listingSchema),
     async (
-        req: Request<
-            Record<string, never>,
-            unknown,
-            CreateListingRequestBody & AuthenticatedBody
-        >,
-        res: Response
+        req: Request<Record<string, never>, Listing, CreateListingRequestBody>,
+        res: Response<Listing>
     ) => {
+        if (!req.auth) {
+            throw new HttpError('Authentication data missing', 401);
+        }
         const { card_id, price, signature, order_hash, order_data, user } =
             req.body;
         const listing = await marketplaceService.createListing({
@@ -81,16 +102,14 @@ router.post(
 // Place a bid on a listing
 router.post(
     '/bid',
-    validateAuth,
     validateSchema(bidSchema),
     async (
-        req: Request<
-            Record<string, never>,
-            unknown,
-            ListedBidRequestBody & AuthenticatedBody
-        >,
-        res: Response
+        req: Request<Record<string, never>, ListingBid, ListedBidRequestBody>,
+        res: Response<ListingBid>
     ) => {
+        if (!req.auth) {
+            throw new HttpError('Authentication data missing', 401);
+        }
         const {
             listing_id,
             bid_amount,
@@ -106,8 +125,8 @@ router.post(
             signature,
             order_hash,
             order_data,
-            bid_type: 'listing', // Adding the missing property
-            bid_amount // Using price as bid_amount
+            bid_type: 'listing',
+            bid_amount
         });
 
         res.status(201).json(bid);
@@ -117,16 +136,14 @@ router.post(
 // Place an open bid on a card
 router.post(
     '/open-bid',
-    validateAuth,
     validateSchema(openBidSchema),
     async (
-        req: Request<
-            Record<string, never>,
-            unknown,
-            OpenBidRequestBody & AuthenticatedBody
-        >,
-        res: Response
+        req: Request<Record<string, never>, OpenBid, OpenBidRequestBody>,
+        res: Response<OpenBid>
     ) => {
+        if (!req.auth) {
+            throw new HttpError('Authentication data missing', 401);
+        }
         const {
             card_id,
             bid_amount,
@@ -153,34 +170,43 @@ router.post(
 );
 
 // Get open bids for a card
-router.get('/cards/:cardId/open-bids', async (req, res) => {
-    const { cardId } = req.params;
-    if (isNaN(parseInt(cardId))) {
-        throw new HttpError('Invalid card ID', 400);
-    }
+router.get(
+    '/cards/:cardId/open-bids',
+    async (
+        req: Request<{ cardId: string }, OrderBook>,
+        res: Response<OrderBook>
+    ) => {
+        const { cardId } = req.params;
+        if (isNaN(parseInt(cardId))) {
+            throw new HttpError('Invalid card ID', 400);
+        }
 
-    const bids = await marketplaceService.getOpenBidsForCard(parseInt(cardId));
-    res.status(200).json(bids);
-});
+        const bids = await marketplaceService.getOpenBidsForCard(
+            parseInt(cardId)
+        );
+        res.status(200).json(bids);
+    }
+);
 
 // Get user's bids (both regular and open)
-router.get('/users/:userId/bids', validateAuth, async (req, res) => {
-    const { userId } = req.params;
-    const bids = await marketplaceService.getAllUserBids(parseInt(userId));
-    res.status(200).json(bids);
-});
+router.get(
+    '/users/:userId/bids',
+    async (
+        req: Request<{ userId: string }, UserBids[]>,
+        res: Response<UserBids[]>
+    ) => {
+        const { userId } = req.params;
+        const bids = await marketplaceService.getAllUserBids(parseInt(userId));
+        res.status(200).json(bids);
+    }
+);
 
 // Accept an open bid
 router.post(
     '/open-bid/:bidId/accept',
-    validateAuth,
     async (
-        req: Request<
-            Record<string, never>,
-            unknown,
-            { transaction_hash: string } & AuthenticatedBody
-        >,
-        res: Response
+        req: Request<{ bidId: string }, OpenBid, { transaction_hash: string }>,
+        res: Response<OpenBid>
     ) => {
         const { bidId } = req.params;
         const { transaction_hash } = req.body;
