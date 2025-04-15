26ff7f1df572fcfdce81186753aee6eb010966a3
wkflanders <wflan@seas.upenn.edu>
2025-04-15 07:23:14 -0400

    cleanup types eslint config

--- packages/types/eslint.config.mjs
+++ packages/types/eslint.config.mjs
@@ -2,16 +2,4 @@ import { baseConfig } from '@phyt/eslint';
 // import { URL } from 'node:url';
 
 /** @type {import("eslint").Linter.Config} */
-export default [
-    ...baseConfig
-    // {
-    //     files: ['**/*.ts', '**/*.tsx'],
-    //     ignores: ['eslint.config.mjs'],
-    //     languageOptions: {
-    //         parserOptions: {
-    //             project: ['./tsconfig.json'],
-    //             tsconfigRootDir: new URL('.', import.meta.url).pathname
-    //         }
-    //     }
-    // },
-];
+export default [...baseConfig];
