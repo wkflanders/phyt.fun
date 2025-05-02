38f6a204a19e1c0ebd1b8ffd4d35c8c816594206
wkflanders <wflan@seas.upenn.edu>
2025-05-01 23:39:57 -0400

    include controller with dep. inj. container

--- apps/server/src/container.ts
+++ apps/server/src/container.ts
@@ -2,10 +2,16 @@ import { makeCommentRepositoryDrizzle } from '@phyt/drizzle';
 
 import { makeCommentService } from '@/services/commentServices.js';
 
+import { makeCommentController } from './controllers/commentController.js';
+
 export const repos = {
     comments: makeCommentRepositoryDrizzle()
 };
 
 export const service = {
     comments: makeCommentService(repos.comments)
+};
+
+export const controller = {
+    comments: makeCommentController(service.comments)
 };
