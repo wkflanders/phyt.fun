e95021f53bed8e8630ee88cca60c6eeb66353aea
wkflanders <wflan@seas.upenn.edu>
2025-05-01 23:42:22 -0400

    update ownership check middleware

--- apps/server/src/middleware/owner.ts
+++ apps/server/src/middleware/owner.ts
@@ -1,25 +1,24 @@
 import { PermissionError } from '@phyt/models';
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-import { validateAuth } from './auth.js';
-
-export const ensureOwnership = [
-    validateAuth,
-    (req: Request, res: Response, next: NextFunction): void => {
-        const { privyId: tokenPrivyId } = req.body as { privyId?: string };
-        if (!tokenPrivyId) {
-            throw new PermissionError('Missing claims payload');
-        }
-
-        const { privyId: paramPrivyId } = req.params;
-        if (!paramPrivyId) {
-            throw new PermissionError('Missing privyId in path');
-        }
-
-        if (paramPrivyId !== tokenPrivyId) {
-            throw new PermissionError('Not authorized to access this resource');
-        }
-
-        next();
+export const ensureOwnership = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): void => {
+    const { privyId: tokenPrivyId } = req.body as { privyId?: string };
+    if (!tokenPrivyId) {
+        throw new PermissionError('Missing claims payload');
     }
-] as RequestHandler[];
+
+    const { privyId: paramPrivyId } = req.params;
+    if (!paramPrivyId) {
+        throw new PermissionError('Missing privyId in path');
+    }
+
+    if (paramPrivyId !== tokenPrivyId) {
+        throw new PermissionError('Not authorized to access this resource');
+    }
+
+    next();
+};
