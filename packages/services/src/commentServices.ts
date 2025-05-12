5c890b6b75da07eceb7fd435c8c6bde961644c44
wkflanders <wflan@seas.upenn.edu>
2025-05-12 04:07:06 -0400

    update comment services

---
+++ packages/services/src/commentServices.ts
@@ -0,0 +1,72 @@
+import { Comment } from '@phyt/models';
+import {
+    UUIDv7,
+    PaginatedComments,
+    CommentData,
+    CreateCommentInput,
+    UpdateCommentInput,
+    CommentQueryParams,
+    NotFoundError,
+    ValidationError
+} from '@phyt/types';
+
+import type { CommentRepository } from '@phyt/repositories';
+
+export type CommentService = ReturnType<typeof makeCommentService>;
+
+export const makeCommentService = (repo: CommentRepository) => {
+    const createComment = async (
+        input: CreateCommentInput
+    ): Promise<CommentData> => {
+        if (!input.content.trim())
+            throw new ValidationError('Cannot be empty comment');
+        Comment.create(input);
+        return repo.create(input);
+    };
+
+    const getPostComments = (
+        postId: UUIDv7,
+        params: CommentQueryParams
+    ): Promise<PaginatedComments> => {
+        return repo.listForPost(postId, params);
+    };
+
+    const getCommentReplies = (
+        commentId: UUIDv7,
+        params: CommentQueryParams
+    ): Promise<PaginatedComments> => {
+        return repo.listReplies(commentId, params);
+    };
+
+    const getCommentById = async (commentId: UUIDv7) => {
+        const comment = await repo.findById(commentId);
+        if (!comment)
+            throw new NotFoundError(`Comment ${String(commentId)} not found`);
+        return comment;
+    };
+
+    const updateComment = async (
+        commentId: UUIDv7,
+        input: UpdateCommentInput
+    ) => {
+        const cur = await repo.findById(commentId);
+        if (!cur) throw new Error('not found');
+        const next = Comment.fromRecord(cur).updateContent({
+            content: input.content
+        });
+        return await repo.update(commentId, next.toJSON());
+    };
+
+    const deleteComment = (commentId: UUIDv7) => {
+        return repo.remove(commentId);
+    };
+
+    return Object.freeze({
+        createComment,
+        getPostComments,
+        getCommentReplies,
+        getCommentById,
+        updateComment,
+        deleteComment
+    });
+};
