6619c7fdd4ba46945f2792b58ea8dfe34d58372e
wkflanders <wflan@seas.upenn.edu>
2025-05-15 11:46:40 -0400

    make comments routes consistent with controllers

--- apps/server/src/routes/comments.ts
+++ apps/server/src/routes/comments.ts
@@ -11,15 +11,15 @@ router.get('/post/:postId', ...controller.comments.getPostComments);
 router.get('/replies/:commentId', ...controller.comments.getCommentReplies);
 
 // Get a specific comment by ID
-router.get('/:id', ...controller.comments.getCommentById);
+router.get('/:commentId', ...controller.comments.getCommentById);
 
 // Create a new comment
 router.post('/', ...controller.comments.createComment);
 
 // Update a comment
-router.patch('/:id', ...controller.comments.updateComment);
+router.patch('/:commentId', ...controller.comments.updateComment);
 
 // Delete a comment
-router.delete('/:id', ...controller.comments.deleteComment);
+router.delete('/:commentId', ...controller.comments.deleteComment);
 
 export { router as commentsRouter };
