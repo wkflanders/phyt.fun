ae45eb54728ffe6a37cccb2eb11c1ad7b92e4317
wkflanders <wflan@seas.upenn.edu>
2025-05-16 08:36:12 -0400

    dep. injection for reactions

--- apps/server/src/container.ts
+++ apps/server/src/container.ts
@@ -4,38 +4,50 @@ import {
     avatarConfig,
     metadataConfig
 } from '@phyt/aws';
-import { makeCommentsDrizzleOps, makeUsersDrizzleOps, db } from '@phyt/drizzle';
+import {
+    makeCommentsDrizzleOps,
+    makeUsersDrizzleOps,
+    makeReactionsDrizzleOps,
+    db
+} from '@phyt/drizzle';
 import {
     makeCommentsRepository,
-    makeUsersRepository
+    makeUsersRepository,
+    makeReactionsRepository
 } from '@phyt/repositories';
 import {
     makeCommentsService,
     makeUsersService,
-    makeMerkleTreeService
+    makeMerkleTreeService,
+    makeReactionsService
 } from '@phyt/services';
 
 import { makeCommentsController } from './controllers/commentsController.js';
+import { makeReactionsController } from './controllers/reactionsController.js';
 import { makeUsersController } from './controllers/usersController.js';
 
 export const ops = {
     commentsDrizzleOps: makeCommentsDrizzleOps(db),
     usersDrizzleOps: makeUsersDrizzleOps(db),
-    userAWSOps: makeUserAWSOps(awsClient, avatarConfig, metadataConfig)
+    userAWSOps: makeUserAWSOps(awsClient, avatarConfig, metadataConfig),
+    reactionsDrizzleOps: makeReactionsDrizzleOps(db)
 };
 
 export const repos = {
     comments: makeCommentsRepository(ops.commentsDrizzleOps),
-    users: makeUsersRepository(ops.usersDrizzleOps, ops.userAWSOps)
+    users: makeUsersRepository(ops.usersDrizzleOps, ops.userAWSOps),
+    reactions: makeReactionsRepository(ops.reactionsDrizzleOps)
 };
 
 export const service = {
     comments: makeCommentsService(repos.comments),
     users: makeUsersService(repos.users),
-    merkletree: makeMerkleTreeService(repos.users)
+    merkletree: makeMerkleTreeService(repos.users),
+    reactions: makeReactionsService(repos.reactions)
 };
 
 export const controller = {
     comments: makeCommentsController(service.comments),
-    users: makeUsersController(service.users)
+    users: makeUsersController(service.users),
+    reactions: makeReactionsController(service.reactions)
 };
