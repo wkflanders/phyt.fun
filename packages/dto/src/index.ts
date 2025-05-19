01b9ad167a2d24412eeea14b603639921fdcf553
wkflanders <wflan@seas.upenn.edu>
2025-05-19 12:26:26 -0400

    DTO barrel file

--- packages/dto/src/index.ts
+++ packages/dto/src/index.ts
@@ -2,6 +2,6 @@ export * from './core.js';
 export * from './usersDTO.js';
 export * from './commentsDTO.js';
 export * from './runnersDTO.js';
-export * from './posts.js';
+export * from './postsDTO.js';
 export * from './reactionsDTO.js';
-export * from './transactions.js';
+export * from './transactionsDTO.js';
