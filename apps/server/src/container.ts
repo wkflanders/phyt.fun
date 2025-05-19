84e10a64665966bd2250b5175e40b2877e5b7177
wkflanders <wflan@seas.upenn.edu>
2025-05-19 12:25:48 -0400

    dependency injection for posts

--- apps/server/src/container.ts
+++ apps/server/src/container.ts
@@ -8,21 +8,25 @@ import {
     makeCommentsDrizzleOps,
     makeUsersDrizzleOps,
     makeReactionsDrizzleOps,
+    makePostsDrizzleOps,
     db
 } from '@phyt/drizzle';
 import {
     makeCommentsRepository,
     makeUsersRepository,
-    makeReactionsRepository
+    makeReactionsRepository,
+    makePostsRepository
 } from '@phyt/repositories';
 import {
     makeCommentsService,
     makeUsersService,
     makeMerkleTreeService,
-    makeReactionsService
+    makeReactionsService,
+    makePostsService
 } from '@phyt/services';
 
 import { makeCommentsController } from './controllers/commentsController.js';
+import { makePostsController } from './controllers/postsController.js';
 import { makeReactionsController } from './controllers/reactionsController.js';
 import { makeUsersController } from './controllers/usersController.js';
 
@@ -30,24 +34,28 @@ export const ops = {
     commentsDrizzleOps: makeCommentsDrizzleOps(db),
     usersDrizzleOps: makeUsersDrizzleOps(db),
     userAWSOps: makeUserAWSOps(awsClient, avatarConfig, metadataConfig),
-    reactionsDrizzleOps: makeReactionsDrizzleOps(db)
+    reactionsDrizzleOps: makeReactionsDrizzleOps(db),
+    postsDrizzleOps: makePostsDrizzleOps(db)
 };
 
 export const repos = {
     comments: makeCommentsRepository(ops.commentsDrizzleOps),
     users: makeUsersRepository(ops.usersDrizzleOps, ops.userAWSOps),
-    reactions: makeReactionsRepository(ops.reactionsDrizzleOps)
+    reactions: makeReactionsRepository(ops.reactionsDrizzleOps),
+    posts: makePostsRepository(ops.postsDrizzleOps)
 };
 
 export const service = {
     comments: makeCommentsService(repos.comments),
     users: makeUsersService(repos.users),
     merkletree: makeMerkleTreeService(repos.users),
-    reactions: makeReactionsService(repos.reactions)
+    reactions: makeReactionsService(repos.reactions),
+    posts: makePostsService(repos.posts)
 };
 
 export const controller = {
     comments: makeCommentsController(service.comments),
     users: makeUsersController(service.users),
-    reactions: makeReactionsController(service.reactions)
+    reactions: makeReactionsController(service.reactions),
+    posts: makePostsController(service.posts)
 };
