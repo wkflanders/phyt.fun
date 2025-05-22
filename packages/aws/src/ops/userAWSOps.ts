2c4f4fd8d31657a335d5a67d4697201cf4a62bc9
wkflanders <wflan@seas.upenn.edu>
2025-05-22 02:31:20 -0400

    split and create metadata aws ops

--- packages/aws/src/ops/userAWSOps.ts
+++ packages/aws/src/ops/userAWSOps.ts
@@ -2,26 +2,18 @@ import { v4 as uuidv4 } from 'uuid';
 import {
     PutObjectCommand,
     DeleteObjectCommand,
-    S3Client,
-    GetObjectCommand
+    S3Client
 } from '@aws-sdk/client-s3';
 
 import { AWSPutError, AWSGetError, AWSDeleteError } from '@phyt/models';
 
-import type {
-    TokenURIMetadata,
-    CardRarity,
-    UUIDv7,
-    AvatarConfig,
-    MetadataConfig
-} from '@phyt/types';
+import type { AvatarConfig } from '@phyt/types';
 
 export type UserAWSOps = ReturnType<typeof makeUserAWSOps>;
 
 export const makeUserAWSOps = (
     s3Client: S3Client,
-    avatarConfig: AvatarConfig,
-    metadataConfig: MetadataConfig
+    avatarConfig: AvatarConfig
 ) => {
     const uploadAvatar = async (file: Buffer): Promise<string> => {
         const bucketName = avatarConfig.bucketName;
@@ -91,67 +83,10 @@ export const makeUserAWSOps = (
         return url.substring(baseUrl.length + 1); // +1 for the trailing slash
     };
 
-    const uploadMetadata = async (
-        tokenId: number,
-        metadata: TokenURIMetadata
-    ): Promise<void> => {
-        try {
-            await s3Client.send(
-                new PutObjectCommand({
-                    Bucket: metadataConfig.bucketName,
-                    Key: tokenId.toString(),
-                    Body: JSON.stringify(metadata),
-                    ContentType: 'application/json'
-                })
-            );
-        } catch (error) {
-            console.error('Error uploading metadata:', error);
-            throw new AWSPutError('Failed to upload metadata to S3');
-        }
-    };
-
-    const getMetadata = async (tokenId: number): Promise<TokenURIMetadata> => {
-        if (!metadataConfig.bucketName) {
-            throw new AWSGetError(
-                'Metadata bucket name not found in configuration'
-            );
-        }
-
-        try {
-            const response = await s3Client.send(
-                new GetObjectCommand({
-                    Bucket: metadataConfig.bucketName,
-                    Key: tokenId.toString()
-                })
-            );
-
-            const bodyContents = await response.Body?.transformToString();
-            if (!bodyContents) {
-                throw new AWSGetError(
-                    `No content found for token ID ${tokenId}`
-                );
-            }
-
-            return JSON.parse(bodyContents) as TokenURIMetadata;
-        } catch (error) {
-            console.error('Error getting metadata:', error);
-            throw new AWSGetError('Failed to get metadata from S3');
-        }
-    };
-
-    const getCardImageUrl = (runnerId: UUIDv7, rarity: CardRarity): string => {
-        return `https://d5mhfgomyfg7p.cloudfront.net/runners/${String(
-            runnerId
-        )}/${String(rarity)}.png`;
-    };
-
     return {
         uploadAvatar,
         generateAvatarUrl,
         deleteAvatar,
-        extractFileKeyFromUrl,
-        uploadMetadata,
-        getMetadata,
-        getCardImageUrl
+        extractFileKeyFromUrl
     };
 };
