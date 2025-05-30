ee74d47e9e7839c9256f4848586e15b1878775c4
wkflanders <wflan@seas.upenn.edu>
2025-05-30 11:17:46 -0400

    refactor and fix frontend lib

--- apps/web/src/lib/wagmi.ts
+++ apps/web/src/lib/wagmi.ts
@@ -1,9 +1,9 @@
+import { env } from '@/env';
+
 import { createConfig } from '@privy-io/wagmi';
 import { base, baseSepolia } from 'viem/chains';
 
 import { http } from 'wagmi';
-
-import { env } from '@/env';
 
 export const config = createConfig({
     chains: [baseSepolia],
