fb6d8244d0cdb1a0517ba4110161e127b86757f0
wkflanders <wflan@seas.upenn.edu>
2025-06-06 12:27:25 -0400

    fix old vitest config import

--- packages/test/vitest.config.ts
+++ packages/test/vitest.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vitest/config';
-import { baseVitestConfig } from './src/base.js';
+import { baseVitestConfig } from './src/config.js';
 
 export default defineConfig(baseVitestConfig);
